c770a22ab7b624988f48d6dad79a3570cdcdabd7
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-25 19:53:45 -0600

    trying different config

--- challenge_4/server/ecosystem.config.js
+++ challenge_4/server/ecosystem.config.js
@@ -10,8 +10,8 @@ module.exports = {
       key: '../minesweeper.pem',
       ref: 'origin/master',
       repo: 'git@github.com:jamesh48/rpt26-mini-apps-2.git',
-      path: '/home/ubuntu/minesweeper/challenge_4',
-      'post-deploy': 'npm install && pm2 startOrRestart challenge_4/server/ecosystem.config.js && pm2 save'
+      path: '/home/ubuntu',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js && pm2 save'
     }
   }
 }
