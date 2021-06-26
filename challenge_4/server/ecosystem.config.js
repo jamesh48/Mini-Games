c46ac5f45e7f378b03deb36d6facf03accb45a01
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-25 19:41:35 -0600

    setup pm2

---
+++ challenge_4/server/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'minesweeper',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: ' ec2-3-140-90-28.us-east-2.compute.amazonaws.com',
+      key: '../minesweeper.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:jamesh48/rpt26-mini-apps-2.git',
+      path: '/home/ubuntu/minesweeper',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js && pm2 save'
+    }
+  }
+}
--- challenge_4/server/index.js
+++ challenge_4/server/index.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const { getResults, postResult } = require('../db/controllers/controllers.js');
+const { getResults, postResult, postNewUser, validateUser } = require('../db/controllers/controllers.js');
 const cors = require('cors');
 
 app.use(cors());
 
 app.use(express.static(path.resolve('public')));
 
-app.get('/topTimes', async ({query: {skillLevel}}, res) => {
-  const topScores = await getResults(skillLevel);
+app.get('/topTimes', async ({ query: { skillLevel, username } }, res) => {
+  const topScores = await getResults(skillLevel, username);
   res.json(topScores);
 })
 
-app.post('/topTimes', async ({query: { skillLevel, timerTime }}, res) => {
+app.get('/validateUser', async ({ query: { userName, userPass } }, res) => {
   try {
-    console.log(skillLevel, timerTime)
-    await postResult(skillLevel, timerTime);
-    res.send('ok');
+    const test = await validateUser(userName, userPass);
+
+    if (!test) {
+      res.send('already exists')
+    } else if (test === 'wrong password') {
+      res.send('wrong password')
+    } else {
+      res.send('does not exist');
+    }
   } catch(err) {
+    console.error(err);
+    res.send(err);
+  }
+});
+
+app.post('/createUser', async ({query: {userName, userPass}}, res) => {
+  const posted = await postNewUser(userName, userPass);
+
+  res.send(posted);
+})
+
+app.post('/topTimes', async ({ query: { skillLevel, timerTime, solidUserName } }, res) => {
+  try {
+
+    await postResult(skillLevel, timerTime, solidUserName);
+    res.send('ok');
+  } catch (err) {
     res.send(err);
   }
 })
