c6e51e6579e1a2e1a1996c97bd7501c22e7839ab
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-25 20:03:31 -0600

    trying to access challenge_4 directly

--- challenge_4/server/ecosystem.config.js
+++ challenge_4/server/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: ' ec2-3-140-90-28.us-east-2.compute.amazonaws.com',
       key: '../minesweeper.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:jamesh48/rpt26-mini-apps-2.git',
+      repo: 'git@github.com:jamesh48/rpt26-mini-apps-2/tree/master/challenge_4.git',
       path: '/home/ubuntu/minesweeper',
       'post-deploy': 'npm install && pm2 startOrRestart ./server/ecosystem.config.js && pm2 save'
     }
