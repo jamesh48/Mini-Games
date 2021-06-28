2c5eb38c5a46ca03b13bcd5b73dba852115de471
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-28 14:12:37 -0600

    images now served from s3

--- challenge_4/src/components/SmileyBar.jsx
+++ challenge_4/src/components/SmileyBar.jsx
@@ -5,7 +5,7 @@ export default ({surprised, flagsRemaining, resetCallback}) => {
 
   return (
     <div id='smiley-bar' style={{alignItems: 'center'}}>
-      <div style={{backgroundImage: `url(${smiley})`}} className='smiley-guy' id={surprised === 'dead' ? 'dead-guy' : surprised === 'victory' ? 'victory-guy' : surprised === true ? 'surprised-guy' : 'smiley-guy'} onClick={() => {resetCallback()}}></div>
+      <div className='smiley-guy' id={surprised === 'dead' ? 'dead-guy' : surprised === 'victory' ? 'victory-guy' : surprised === true ? 'surprised-guy' : 'smiley-guy'} onClick={() => {resetCallback()}}></div>
       <div id='flags-remaining'>{flagsRemaining}</div>
     </div>
   )
