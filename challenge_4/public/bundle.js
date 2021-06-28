bcb863d7f6d00c82db1f61a75f91db7239c1991b
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-28 12:28:02 -0600

    changed target id for proxy purposes

--- challenge_4/public/bundle.js
+++ challenge_4/public/bundle.js
@@ -389,7 +389,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var _components_app_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/app.jsx */ \"./src/components/app.jsx\");\n\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_app_jsx__WEBPACK_IMPORTED_MODULE_2__.default, null), document.getElementById('root'));\n\n//# sourceURL=webpack://challenge_4/./src/index.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var _components_app_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/app.jsx */ \"./src/components/app.jsx\");\n\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_app_jsx__WEBPACK_IMPORTED_MODULE_2__.default, null), document.getElementById('minesweeper-root'));\n\n//# sourceURL=webpack://challenge_4/./src/index.jsx?");
 
 /***/ }),
 
--- challenge_4/src/index.jsx
+++ challenge_4/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import App from './components/app.jsx';
 
-reactDOM.render(<App />, document.getElementById('root'));
+reactDOM.render(<App />, document.getElementById('minesweeper-root'));
