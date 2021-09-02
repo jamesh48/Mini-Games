8fcd01d4dbca4311dff0ff8a9933139a6f2a7f4d
James Hrivnak <jameshrivnak4@gmail.com>
2021-09-02 02:46:41 -0600

    making changes for deployment

--- challenge_4/server/ecosystem.config.js
+++ challenge_4/server/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: ' ec2-3-140-90-28.us-east-2.compute.amazonaws.com',
       key: './minesweeper.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:jamesh48/rpt26-mini-apps-2.git',
+      repo: 'git@github.com:jamesh48/mini-apps-2.git',
       path: '/home/ubuntu/minesweeper',
       'post-deploy': 'cd /home/ubuntu/minesweeper/source/challenge_4 && npm install && npm run build && pm2 startOrRestart ./server/ecosystem.config.js && pm2 save'
     }
